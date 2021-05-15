66992697f2fee7e0cb5c8a67aab99ff8e443fd93
Akkisdiary <shegaonkar.akshay2@gmail.com>
2021-05-15 18:25:58 +0530

    feat: add checkbox validation style: add validation feedback for users

--- cowin_alerts/index.py
+++ cowin_alerts/index.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request, url_for
+from werkzeug.utils import redirect
 
 from .forms import SubscribeForm
 from .models import Subscribers, db
@@ -15,7 +16,7 @@ scheduler.start()
 
 
 index_bp = Blueprint(
-    'index_bp', 
+    'index_bp',
     __name__,
     template_folder='templates',
     static_folder='static'
@@ -25,25 +26,28 @@ index_bp = Blueprint(
 @index_bp.route('/', methods=['GET', 'POST'])
 def index():
     sub_form = SubscribeForm()
+    success = request.args.get('success') or False
 
     if sub_form.validate_on_submit():
-        subscriber = Subscribers(
-            name=sub_form.name.data.strip(),
-            email=sub_form.email.data,
-            pincode=sub_form.pincode.data,
-            sub_18=sub_form.sub_18.data,
-            sub_45=sub_form.sub_45.data,
-        )
+        if not sub_form.sub_18.data and not sub_form.sub_45.data:
+            flash('Please select at least 1 age group')
+        else:
+            subscriber = Subscribers(
+                name=sub_form.name.data.strip(),
+                email=sub_form.email.data,
+                pincode=sub_form.pincode.data,
+                sub_18=sub_form.sub_18.data,
+                sub_45=sub_form.sub_45.data,
+            )
 
-        db.session.add(subscriber)
-        db.session.commit()
+            db.session.add(subscriber)
+            db.session.commit()
 
-        return render_template(
-            'success.html',
-        )
+            return redirect(url_for('index_bp.index', success=True))
 
     return render_template(
         'index.html',
         title='Cowin Alerts',
-        sub_form=sub_form
+        sub_form=sub_form,
+        success=success,
     )
