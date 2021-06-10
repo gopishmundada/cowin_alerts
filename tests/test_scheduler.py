47f640c48d1a19eb788c6a6a079c0cfd74d26b4c
Akkisdiary <shegaonkar.akshay2@gmail.com>
2021-06-10 21:02:45 +0530

    implement get_recipients_list() add test cases for the same

---
+++ tests/test_scheduler.py
@@ -0,0 +1,60 @@
+from cowin_alerts.check_slots import get_subscribers_list
+from cowin_alerts.models import (Pincodes, Preference,
+                                 SubscriberPincodePreferences, Subscribers, db)
+
+
+def test_getRecipientsList(app):
+    district1 = Pincodes(pincode=431203)
+    district2 = Pincodes(pincode=431204)
+    user1 = Subscribers(email='tony@stark.com', name='Ironman')
+
+    db.session.add_all([district1, district2, user1])
+    db.session.commit()
+
+    subscriptions = [
+        SubscriberPincodePreferences(
+            subscriber=user1,
+            pincode=district1,
+            preference=Preference.get_or_create(True, True),
+        ),
+        SubscriberPincodePreferences(
+            subscriber=Subscribers(email='arya@stark.com', name='Needle'),
+            pincode=district1,
+            preference=Preference.get_or_create(True, False),
+        ),
+        SubscriberPincodePreferences(
+            subscriber=user1,
+            pincode=district2,
+            preference=Preference.get_or_create(True, True),
+        ),
+        SubscriberPincodePreferences(
+            subscriber=Subscribers(email='maha@dev.com', name='Bhole'),
+            pincode=district1,
+            preference=Preference.get_or_create(True, True),
+        ),
+    ]
+
+    db.session.add_all(subscriptions)
+    db.session.commit()
+
+    jalna = Pincodes.query.filter(Pincodes.pincode == 431203).first()
+
+    # Strict conditions
+    assert [
+        ('Ironman', 'tony@stark.com'),
+        ('Bhole', 'maha@dev.com'),
+    ] == get_subscribers_list(jalna, True, True)
+    assert [('Needle', 'arya@stark.com')
+            ] == get_subscribers_list(jalna, True, False)
+    assert [] == get_subscribers_list(jalna, False, True)
+
+    # Non strict conditions
+    assert [
+        ('Ironman', 'tony@stark.com'),
+        ('Bhole', 'maha@dev.com'),
+        ('Needle', 'arya@stark.com'),
+    ] == get_subscribers_list(jalna, True, None)
+    assert [
+        ('Ironman', 'tony@stark.com'),
+        ('Bhole', 'maha@dev.com'),
+    ] == get_subscribers_list(jalna, None, True)
